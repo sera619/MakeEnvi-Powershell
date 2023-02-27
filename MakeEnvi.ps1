dc8fe17263034fb785a52d4a8f1ee11eb25d6f70
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-02-27 16:25:58 +0100

    add checks for installed py and code

--- MakeEnvi.ps1
+++ MakeEnvi.ps1
@@ -1,18 +1,69 @@
 
+
 function MakeEnvi {
     param (
         $lang, $name
     )
-    Write-Host ""
-    if ($lang -and $name){
-        # Set this to your Codedirectory
-        $codedir = "<codedirectory>"
-        #
-        if ($codedir -eq "<codedirectory>"){
-            Write-Host -fore DarkRed "[x] ERROR: Please change the codedir variable in the script!"
+    # Set this to your Codedirectory
+    $codedir = "<codedirectory>"
+    ######   
+    if ($codedir -eq "<codedirectory>"){
+        Write-Host -fore DarkRed [x] Error!
+        Write-Host -fore DarkRed "[x] You didnt change the Code-Directory variable in file."
+        Write-Host -fore DarkRed "[x] Exit !"
+        explorer $PROFILE
+        return
+    }
+    $python = ""
+    $vscode = ""
+    $InstalledSoftware = Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+    foreach($obj in $InstalledSoftware){
+        #write-host $obj.GetValue('DisplayName') -NoNewline; write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')
+        if ($obj.GetValue('DisplayName') -like "*Python*") {
+            if ($python -eq ""){
+                $python = "Ok"
+            }
+        }
+        if ($obj.GetValue('DisplayName') -like "*Studio Code*") {
+            if ($vscode -eq ""){
+                $vscode = "Ok"
+            }
+        }
+    }
+
+    if ((!$python -eq "Ok" )-or(!$vscode -eq "Ok")){
+        if (!$python -eq "Ok"){
+            Write-Host -fore DarkRed [x] Error!
+            Write-Host -fore DarkRed "[x] No Python found! Install please!"
             Write-Host -fore DarkRed "[x] Exit !"
             return
         }
+        if (!$vscode -eq "Ok"){
+            Write-Host -fore DarkRed [x] Error!
+            Write-Host -fore DarkRed "[x] No Visual Studio Code found! Install please!"
+            Write-Host -fore DarkRed "[x] Exit !"
+            return
+        }
+    }
+    Write-Host -fore Green "[!] Python found!"
+    Write-Host -fore Green "[!] Visual Studio Code found!"
+    Write-Host ""
+    
+    if (!$lang -or !$name){
+        Write-Host -fore DarkRed [x] Error!
+        if (!$lang){
+            Write-Host -fore DarkRed [x] Language not set!
+        }
+        if (!$name){
+            Write-Host -fore DarkRed [x] Projectname not set!
+        }
+        Write-Host -fore DarkRed "[x] Exit !"
+        Write-Host ""
+        return       
+    }
+    Write-Host ""
+    if ($lang -and $name){
+
         $lang = (Get-Culture).TextInfo.ToTitleCase( $lang.ToLower())
         if($lang -eq "Python"){
             Write-Host -fore Cyan `#`#`#`#`#`#` MakeEnvi `- Python Project `#`#`#`#`#`#`
@@ -68,17 +119,6 @@ function MakeEnvi {
             
         }
         Write-Host -fore Yellow ""
-    }else{
-        Write-Host -fore DarkRed [x] Error!
-        if (!$lang){
-            Write-Host -fore DarkRed [x] Langquage not set!
-        }
-        if (!$name){
-            Write-Host -fore DarkRed [x] Langquage not set!
-        }
-        Write-Host -fore DarkRed "[x] Exit !"
-        Write-Host ""
-        return       
     }
 }   
 
