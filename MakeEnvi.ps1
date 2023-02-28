f2c939265951ca5459f117c127780e57914cfa7d
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-02-28 18:18:05 +0100

    create base JS/ write html temp

--- MakeEnvi.ps1
+++ MakeEnvi.ps1
@@ -7,7 +7,7 @@ function MakeEnvi {
     # Set this to your Codedirectory
     $codedir = "<codedirectory>"
     ######   
-    if ($codedir -eq "<codedirectory>"){
+    if ($codedir -eq "<codedirectory>") {
         Write-Host -fore DarkRed [x] Error!
         Write-Host -fore DarkRed "[x] You didnt change the Code-Directory variable in file."
         Write-Host -fore DarkRed "[x] Exit !"
@@ -17,30 +17,30 @@ function MakeEnvi {
     $python = ""
     $vscode = ""
     $InstalledSoftware = Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-    foreach($obj in $InstalledSoftware){
+    foreach ($obj in $InstalledSoftware) {
         #write-host $obj.GetValue('DisplayName') -NoNewline; write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')
         if ($obj.GetValue('DisplayName') -like "*Python*") {
-            if ($python -eq ""){
+            if ($python -eq "") {
                 $python = "Ok"
             }
         }
         if ($obj.GetValue('DisplayName') -like "*Studio Code*") {
-            if ($vscode -eq ""){
+            if ($vscode -eq "") {
                 $vscode = "Ok"
             }
         }
     }
 
-    if ((!$python -eq "Ok" )-or(!$vscode -eq "Ok")){
-        if (!$python -eq "Ok"){
+    if ((!$python -eq "Ok" ) -or (!$vscode -eq "Ok")) {
+        if (!$python -eq "Ok") {
             Write-Host -fore DarkRed [x] Error!
-            Write-Host -fore DarkRed "[x] No Python found! Install please!"
+            Write-Host -fore DarkRed "[x] No Python found! Install please! (https://www.python.org/downloads/)"
             Write-Host -fore DarkRed "[x] Exit !"
             return
         }
-        if (!$vscode -eq "Ok"){
+        if (!$vscode -eq "Ok") {
             Write-Host -fore DarkRed [x] Error!
-            Write-Host -fore DarkRed "[x] No Visual Studio Code found! Install please!"
+            Write-Host -fore DarkRed "[x] No Visual Studio Code found! Install please!(https://code.visualstudio.com/Download)"
             Write-Host -fore DarkRed "[x] Exit !"
             return
         }
@@ -48,13 +48,13 @@ function MakeEnvi {
     Write-Host -fore Green "[!] Python found!"
     Write-Host -fore Green "[!] Visual Studio Code found!"
     Write-Host ""
-    
-    if (!$lang -or !$name){
+
+    if (!$lang -or !$name) {
         Write-Host -fore DarkRed [x] Error!
-        if (!$lang){
+        if (!$lang) {
             Write-Host -fore DarkRed [x] Language not set!
         }
-        if (!$name){
+        if (!$name) {
             Write-Host -fore DarkRed [x] Projectname not set!
         }
         Write-Host -fore DarkRed "[x] Exit !"
@@ -62,19 +62,20 @@ function MakeEnvi {
         return       
     }
     Write-Host ""
-    if ($lang -and $name){
+    if ($lang -and $name) {
 
         $lang = (Get-Culture).TextInfo.ToTitleCase( $lang.ToLower())
-        if($lang -eq "Python"){
+        if ($lang -eq "Python") {
             Write-Host -fore Cyan `#`#`#`#`#`#` MakeEnvi `- Python Project `#`#`#`#`#`#`
-            
-            $path = "C:\"+$codedir+"\"+$lang+"\"+$name
+        
+            $path = "C:\" + $codedir + "\" + $lang + "\" + $name
             $exist = Test-Path $path
-            if(!$exist){
+            if (!$exist) {
                 Write-Host -fore Yellow [!] Erstelle $path ...
-                
+            
                 New-Item -Type Directory $path | Out-Null
-            }else{
+            }
+            else {
                 Write-Host -fore DarkRed [x] Error: Pfad $path existiert bereits!
                 return
             }
@@ -85,17 +86,19 @@ function MakeEnvi {
             Write-Host -fore Yellow ""
             Write-Host -fore Yellow [!] Aktiviere Python Environment: $name
             & env-$name\Scripts\Activate
-        }elseif ($lang -eq "Javascript"){
+        }
+        elseif ($lang -eq "Javascript") {
             Write-Host -fore Yellow ""
             Write-Host -fore Cyan `#`#`#`#`#`#` MakeEnvi `- JavaScript Project `#`#`#`#`#`#`
-            
-            $path = "C:\"+$codedir+"\"+$lang+"\"+$name
+        
+            $path = "C:\" + $codedir + "\" + $lang + "\" + $name
             $exist = Test-Path $path
-            if(!$exist){
+            if (!$exist) {
                 Write-Host -fore Yellow [!] Erstelle $path ...
-                
+            
                 New-Item -Type Directory $path | Out-Null
-            }else{
+            }
+            else {
                 Write-Host -fore DarkRed [x] Error: Pfad $path existiert bereits!
                 Write-Host -fore DarkRed "[x] Exit !"
                 return
@@ -105,18 +108,25 @@ function MakeEnvi {
         Write-Host -fore Yellow ""
         Write-Host -fore Cyan "[?] Soll ein Basic-Filesystem erstellt werden? (y/n):"
         $option = Read-Host
-        if ($option -eq "y"){
+        if ($option -eq "y") {
             Write-Host -fore Yellow ""
             Write-Host -fore Yellow [!] Erstelle Basic-Filesystem...
             Write-Host -fore Yellow ""
-            CBF
+            if ($lang -eq "Javascript") {
+                createJSFiles
+            }
+            elseif ($lang -eq "Python") {
+                CBF
+            }
+
             Write-Host -fore Yellow ""
             Write-Host -fore Green [!] Neues Project erstellt!
             code .
-        } else {
+        }
+        else {
             Write-Host -fore Yellow ""
             Write-Host -fore Green [!] Neues Project erstellt!
-            
+        
         }
         Write-Host -fore Yellow ""
     }
@@ -124,6 +134,12 @@ function MakeEnvi {
 
 Set-Alias shortcut `MakeEnvi`
 
+function createJSFiles {
+    New-Item -Type Directory script, styles, tests, src, assets, assets/img, assets/audio | Out-Null
+    New-Item  script/main.js, styles/main.css, main.html | Out-Null
+    WriteReadme -name $name
+    WriteHTML
+}
 function CBF {
     New-Item -Type Directory tests, src, assets, assets/img, assets/audio | Out-Null
     New-Item main.py, src/settings.py, src/__init__.py, README.md, .gitignore, config.yml | Out-Null
@@ -133,12 +149,30 @@ function CBF {
 Set-Alias shortcut `CBF`
 
 
-function WriteReadme{
+function WriteHTML {
+    $text = "<!DOCTYPE html>
+<html lang='EN'>
+<head>
+    <link rel='stylesheet' href='styles/main.css'>
+    <title>$name</title>
+</head>
+
+<body>
+    <h1>New Project @ S3R43o3</h2>
+    <br>
+    <p>Please change the template at 'main.html' or 'scripts/main.js'!</p>
+    <script src='scripts/main.js'></script>
+</body>
+<html>"
+    $text | Out-File -FilePath main.html | Out-Null
+}
+
+function WriteReadme {
     param (
         $path,
         $name
     )
-    
+
     $text = "# $name #
 
 > Set your Projectdescription text!
@@ -146,15 +180,14 @@ function WriteReadme{
 ## Installation ##
 
 > Set your installation instructions here."
-
     $text | Out-File -FilePath README.md | Out-Null
-
 }
 
-function WriteConfig{
+function WriteConfig {
     param(
         $name
     )
-    $text = "name: " + $name + "`nversion: 0.1"
-    $text | Out-File -FilePath .\config.yml | Out-Null
+    $text = "name: " + $name + "`nversion: 0.1`n"
+
+    $text | Out-File -FilePath config.yml | Out-Null 
 }
